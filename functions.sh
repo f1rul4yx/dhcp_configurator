0aabf5801bccc89072663e29b21711115c77031c
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2025-04-02 10:18:20 +0200

    Creación del archivo functions.sh

---
+++ functions.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+#Author: Diego Vargas
+#Fecha de creacion: abr 02 2025
+#Version: 1.0
+
+#--------------------VARIABLES--------------------#
+
+# COLORES
+
+# Resetear todos los atributos
+RESET="\e[0m"
+
+# Estilos
+NEGRITA="\e[1m"
+ATENUADO="\e[2m"
+CURSIVA="\e[3m"
+SUBRAYADO="\e[4m"
+PARPADEO="\e[5m"
+PARPADEO_INTENSO="\e[6m"
+INVERTIDO="\e[7m"
+OCULTO="\e[8m"
+TACHADO="\e[9m"
+
+# Colores de texto
+NEGRO="\e[30m"
+ROJO="\e[31m"
+VERDE="\e[32m"
+AMARILLO="\e[33m"
+AZUL="\e[34m"
+MORADO="\e[35m"
+CIAN="\e[36m"
+GRIS="\e[37m"
+BLANCO="\e[38m"
+
+# Colores de fondo
+FONDO_NEGRO="\e[40m"
+FONDO_ROJO="\e[41m"
+FONDO_VERDE="\e[42m"
+FONDO_AMARILLO="\e[43m"
+FONDO_AZUL="\e[44m"
+FONDO_MORADO="\e[45m"
+FONDO_CIAN="\e[46m"
+FONDO_GRIS="\e[47m"
+FONDO_BLANCO="\e[48m"
+
+#--------------------FUNCIONES--------------------#
